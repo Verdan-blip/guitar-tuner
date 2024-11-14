0442505d70be2129c60b6c6138089d79f0fe5412
islam <islam.bagaviev.2014@mail.ru>
2024-11-14 21:12:32 +0300

    Added base libraries, created feature-tuner module

--- feature-tuner/build.gradle.kts
+++ feature-tuner/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "ru.muztache.guitartuner"
+    namespace = "ru.muztache.feature.tuner"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "ru.muztache.guitartuner"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(project(":core-common"))
     implementation(project(":core-util"))
     implementation(project(":core-theme"))
